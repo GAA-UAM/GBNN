b9149453553d6f8140f1729486889d1c6e4c2f4c
Seyedsaman (Sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-02-02 23:52:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="GBNN",
-    version="0.0.1",
+    version="0.0.2",
     author="Seyedsaman Emami, Gonzalo Martínez-Muñoz",
     author_email="emami.seyedsaman@uam.es, gonzalo.martinez@uam.es",
     description="Gradient Boosted Neural Network",
