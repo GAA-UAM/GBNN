b8992827c4793f8be620118b6a3340fef42c8171
Seyedsaman (Sam) Emami <54855771+samanemami@users.noreply.github.com>
2022-05-19 11:39:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 import codecs
-import 
+import os
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,20 +12,19 @@ VERSION = '0.0.2'
 DESCRIPTION = 'GBNN'
 LONG_DESCRIPTION = 'Gradient Boosted Neural Network.'
 
-setuptools.setup(
-    name="GBNN",
-    version=VERSION,
-    author="Seyedsaman Emami, Gonzalo Martínez-Muñoz",
-    author_email="emami.seyedsaman@uam.es, gonzalo.martinez@uam.es",
-    description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    install_requires=['numpy', 'scipy', 'sklearn'],
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
-)
+setup(name="GBNN",
+      version=VERSION,
+      author="Seyedsaman Emami, Gonzalo Martínez-Muñoz",
+      author_email="emami.seyedsaman@uam.es, gonzalo.martinez@uam.es",
+      description=LONG_DESCRIPTION,
+      packages=find_packages(),
+      install_requires=['numpy', 'scipy', 'sklearn'],
+      classifiers=[
+          "Development Status :: 1 - Planning",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python :: 3",
+          "Operating System :: Unix",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows",
+      ]
+      )
